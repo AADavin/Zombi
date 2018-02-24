21dec253f0e96ce8e888599810035669f2dc2e18
AADavin <aaredav@gmail.com>
2018-02-24 12:58:05 +0100

    Saving progress

--- NewGenome.py
+++ NewGenome.py
@@ -1,5 +1,15 @@
+import AuxiliarFunctions as af
 import numpy
 import copy
+
+import ete3
+
+from ete3 import Tree
+
+input = [(10, "C"), (10, "C")]
+all_gene_families = dict()
+species_counter = 0
+
 
 def copy_segment(segment, new_identifiers):
 
@@ -18,7 +28,6 @@ def invert_segment(segment):
         gene.change_sense()
 
 
-
 def return_new_identifiers_for_segment(segment, all_gene_families):
 
     new_identifiers = list()
@@ -33,29 +42,19 @@ def return_new_identifiers_for_segment(segment, all_gene_families):
 
 class GeneFamily():
 
-    def __init__(self, identifier):
+    def __init__(self, identifier, time):
 
         self.identifier = identifier
+        self.origin = time
+
         self.genes = list()
         self.events = list()
         self.event_counter = 0  # Each time that the family is modified in any form, we have to update the event counter
         self.gene_ids_counter = 0
 
-    def register_loss(self, time, genes):
+    def register_event(self, time, event, genes):
 
-        self.events.append((time, "L", genes))
-
-    def register_duplication(self, time, genes):
-
-        self.events.append((time, "D", genes))
-
-    def register_inversion(self, time, genes):
-
-        self.events.append((time, "I", genes))
-
-    def register_translocation(self, time, genes):
-
-        self.events.append((time, "C", genes))
+        self.events.append((time, event, genes))
 
     def generate_tree(self):
         pass
@@ -111,6 +110,7 @@ class Chromosome():
         self.genes = list()
         self.intergene_distances = list()
         self.shape = shape
+        self.species = ""
 
     def select_random_position(self):
 
@@ -127,6 +127,12 @@ class Chromosome():
     def __str__(self):
 
         return ";".join([str(gene) for gene in self.genes])
+
+    def __iter__(self):
+
+        for x in self.genes:
+            yield x
+
 
 class CircularChromosome(Chromosome):
 
@@ -195,55 +201,64 @@ class Genome():
 
     def select_random_chromosome(self):
 
-        # I have to ponderate by the length of each chromosome
+        # I have to weight by the length of each chromosome
 
-        return numpy.random.randint(len(self.genes))
+        chromosome = numpy.random.choice([self.chromosomes], 1, p=af.normalize([len(x) for x in self.chromosomes]))
 
-
-
-
+        return chromosome
 
 
-input = [(10, "C"), (10, "C")]
+    def __iter__(self):
+        for chromosome in self.chromosomes:
+            yield chromosome
 
-# First we create genes and their gene families
-# A gene belongs both to a gene family and to a chromosome
-# So first you create chromosomes and then they create gene families
 
-gene_families_counter = 0
-all_gene_families = dict()
-genome = Genome()
-gf_name_id = 0
+def make_speciation(sp, c1, c2):
 
-for n_genes, shape in input:
+    genome1 = Genome()
+    genome2 = Genome()
 
-    # We create a chromosome for each entry
+    genome1.species = c1.name
+    genome2.species = c2.name
 
-    if shape == "L":
-        chromosome = LinearChromosome(shape)
-    elif shape == "C":
-        chromosome = CircularChromosome(shape)
+    for chromosome in sp.genome:
 
-    for i in range(n_genes):
-        # We fill the chromosomes and we create also the gene families
+        shape = chromosome.shape
 
-        gf_name_id += 1
-        gene_family = GeneFamily(gf_name_id)
+        genome1.chromosomes.append(Chromosome(shape))
+        genome2.chromosomes.append(Chromosome(shape))
 
-        gene = Gene()
-        gene.determine_orientation()
-        gene.gene_family = gf_name_id
+        for gene in chromosome:
 
-        gene.gene_id = gene_family.obtain_new_gene_id()
-        gene.genome = "Root"
+            new_id1 = return_new_identifiers_for_segment(gene, all_gene_families)
+            new_id2 = return_new_identifiers_for_segment(gene, all_gene_families)
 
-        gene_family.genes.append(gene)
-        chromosome.genes.append(gene)
-        all_gene_families[gf_name_id] = gene_family
+            new_gene1 = copy_segment(Gene(), new_id1)
+            new_gene2 = copy_segment(Gene(), new_id2)
+
+            genome1.chromosomes.genes.append(new_gene1)
+            genome2.chromosomes.genes.append(new_gene2)
+
+            gene.active = False
+
+    return genome1, genome2
 
 
 
-def make_loss(p):
+
+def make_extinction(sp):
+
+    # We have to inactivate all the genes
+
+    genome = sp.genome
+
+    for chromosome in genome:
+        for gene in chromosome:
+            gene.active = False
+
+
+
+def make_loss(p, chromosome):
 
     affected_genes = chromosome.obtain_affected_genes(p)
     segment = chromosome.obtain_segment(affected_genes)
@@ -254,10 +269,10 @@ def make_loss(p):
 
     for gene in segment:
         gene.active = False
-        all_gene_families[gene.gene_family].register_loss(0, gene.gene_id)
+        all_gene_families[gene.gene_family].register_event(0, "L", gene.gene_id)
 
 
-def make_duplication(p):
+def make_duplication(p, chromosome):
 
     affected_genes = chromosome.obtain_affected_genes(p)
     segment = chromosome.obtain_segment(affected_genes)
@@ -269,8 +284,6 @@ def make_duplication(p):
 
     copied_segment1 = copy_segment(segment, new_identifiers1)
     copied_segment2 = copy_segment(segment, new_identifiers2)
-
-    # We remove the old segment
 
     # We insert the two new segments after the last position of the old segment
 
@@ -284,9 +297,10 @@ def make_duplication(p):
 
     for i,gene in enumerate(segment):
         gene.active = False
-        all_gene_families[gene.gene_family].register_duplication(0,(gene.gene_id, new_identifiers1[i], new_identifiers2[i]))
+        all_gene_families[gene.gene_family].register_event(0, "D", ";".join(
+            (gene.gene_id, new_identifiers1[i], new_identifiers2[i])))
 
-def make_inversion(p):
+def make_inversion(p, chromosome):
 
     affected_genes = chromosome.obtain_affected_genes(p)
     segment = chromosome.obtain_segment(affected_genes)
@@ -300,45 +314,153 @@ def make_inversion(p):
 
     for i,gene in enumerate(segment):
         gene.active = False
-        all_gene_families[gene.gene_family].register_inversion(0, str(gene.gene_id) + ";" + str(new_identifiers[i]))
+        all_gene_families[gene.gene_family].register_event(0, "I", str(gene.gene_id) + ";" + str(new_identifiers[i]))
 
 
-def make_translocation():
-    
+def make_translocation(p, chromosome):
+
     affected_genes = chromosome.obtain_affected_genes(p)
     segment = chromosome.obtain_segment(affected_genes)
 
     new_identifiers = return_new_identifiers_for_segment(segment, all_gene_families)
     new_segment = copy_segment(segment, new_identifiers)
-    invert_segment(new_segment)
 
-    chromosome.insert_segment(affected_genes[0], new_segment[::-1])
+    # For now all translocations keep the orientation of the DNA
+
+    position = chromosome.select_random_position()
+
+    # Watch out! Right now translocations can occur within the sequence (resulting in no changes at all)
+
+    chromosome.insert_segment(position, new_segment)
     chromosome.remove_segment(segment)
 
     for i, gene in enumerate(segment):
         gene.active = False
-        all_gene_families[gene.gene_family].register_inversion(0, str(gene.gene_id) + ";" + str(new_identifiers[i]))
+        all_gene_families[gene.gene_family].register_event(0, "C",
+                                                           str(gene.gene_id) + ";" + str(new_identifiers[i]))
+
+def make_origination(gene_family_id, species_tree_node):
+
+    gene = Gene()
+    gene.determine_orientation()
+    gene_family_id += 1
+    gene.gene_family = gene_family_id
+
+    gene_family = GeneFamily(gene_family_id, 0)
+
+    gene.determine_orientation()
+    gene.gene_family = gene_family_id
+    gene.gene_id = gene_family.obtain_new_gene_id()
+    gene.genome = species_tree_node
+    gene_family.genes.append(gene)
+    all_gene_families[gene_family_id] = gene_family
+
+    return gene, gene_family
 
 
-def make_origination():
-    pass
+def make_transfer(p, chromosome1, chromosome2):
+
+    affected_genes = chromosome1.obtain_affected_genes(p)
+    segment = chromosome1.obtain_segment(affected_genes)
+
+    new_identifiers1 = return_new_identifiers_for_segment(segment, all_gene_families)
+    new_identifiers2 = return_new_identifiers_for_segment(segment, all_gene_families)
+
+    # Now we create two segments
+
+    copied_segment1 = copy_segment(segment, new_identifiers1)
+    copied_segment2 = copy_segment(segment, new_identifiers2)
+
+    # We insert the first segment (leaving transfer) in the same position than the previous segment
+    # We do this just to change the identifiers of the numbers
+
+    chromosome1.insert_segment(affected_genes[0], copied_segment1)
+
+    # And we remove the old segment
+
+    chromosome1.remove_segment(segment)
+
+    # Now we insert the transfer segment in the recipient genome
+
+    position = chromosome2.select_random_position()
+
+    chromosome2.insert_segment(position, copied_segment2)
+
+    # We have to register in the affected gene families that there has been a transfer event
+
+    for i, gene in enumerate(segment):
+
+        gene.active = False
+
+        # The code for the node is:
+        # 1. Branch of the species tree for the donor genome
+        # 2. Id of the gene that is transferred
+        # 3. Id of the gene that remains in the donor genome
+        # 4. Branch of the species tree for the recipient genome
+        # 5. Id of the new gene arriving
+
+        nodes = [chromosome1.species,
+                 segment[i].gene_id,
+                 new_identifiers1[i],
+                 chromosome2.species,
+                 new_identifiers2[i],
+                 ]
+
+        all_gene_families[gene.gene_family].register_event(0, "T", ";".join(nodes))
 
 
-def make_transfer():
-    pass
+# First we create genes and their gene families
+# A gene belongs both to a gene family and to a chromosome
+# So first you create chromosomes and then they create gene families
+
+def GenomeEvolver():
+
+    tree = ete3.Tree()
+    genome = Genome()
+    tree.name = "Root"
+    tree.add_feature("Genome", genome)
+    sp = tree.get_tree_root()
+
+    gene_family_id = 0
+
+    for n_genes, shape in input:
+
+        # We create a chromosome for each entry
+
+        if shape == "L":
+            chromosome = LinearChromosome(shape)
+        elif shape == "C":
+            chromosome = CircularChromosome(shape)
+
+        chromosome.species = "Root"
+
+        for i in range(n_genes):
+
+            # We fill the chromosomes and we create also the gene families
+
+            gene_family_id += 1
+            gene, gene_family = make_origination(gene_family_id, chromosome.species)
+            chromosome.genes.append(gene)
+            all_gene_families[gene_family_id] = gene_family
+
+
+    c1 = tree.add_child(dist=0)
+
+    c1.name = "n1"
+    c2 = tree.add_child(dist=0)
+    c2.name = "n2"
+
+    g1,g2 = make_speciation(sp,c1,c2)
+
+
+## Testing
 
 
 
-print(chromosome)
-make_inversion(0.4)
-print(chromosome)
 
 
 
-
-
-
-
+GenomeEvolver()
 
 
 
@@ -351,38 +473,3 @@ print(chromosome)
 #print(chromosome)
 
 
-
-'''    
-# THIS IS WORKING
-
-for x in all_gene_families:
-    print(str(all_gene_families[x]))
-
-print("NOW WE CHANGE")
-
-chromosome.genes[3].gene_id = "4"
-
-for x in all_gene_families:
-    print(str(all_gene_families[x]))
-
-
-print(chromosome)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#genome.start_genome(input)
-
-'''
